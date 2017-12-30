73326212ef488ccf72aa47c5a9ba30b5ef2209a7
Scott Bailey <scott@zeddic.com>
2017-12-29 17:09:07 -0800

    Switches to P2 physics from Arcade physics

--- src/game.ts
+++ src/game.ts
@@ -14,31 +14,34 @@ function preload () {
 }
 
 function create () {
+
+	game.physics.startSystem(Phaser.Physics.P2JS);
+  game.physics.p2.gravity.y = 80;
+  game.physics.p2.restitution = 0.8;
+
+
   ship = game.add.sprite(game.world.centerX, game.world.centerY, Asset.SHIP);
   ship.anchor.setTo(0.5, 0.5);
 
-  game.physics.enable(ship, Phaser.Physics.ARCADE);
-  game.physics.arcade.gravity.y = 50;
+  game.physics.enable(ship, Phaser.Physics.P2JS, true);
 
+  // let points = [
+  //   0, 0,
+  //   game.world.width, 0,
+  //   game.world.width, 50,
+  //   0, 50,
+  // ];
 
-  let points = [
-    0, 0,
-    game.world.width, 0,
-    game.world.width, 50,
-    0, 50,
-  ];
+  // poly = new Phaser.Polygon(points);
+  // graphics = game.add.graphics(0, game.world.height - 50);
+  // graphics.beginFill(0xFFFF00);
+  // graphics.drawPolygon(poly.points);
+  // graphics.endFill();
 
-  poly = new Phaser.Polygon(points);
-  graphics = game.add.graphics(0, game.world.height - 50);
-  graphics.beginFill(0xFFFF00);
-  graphics.drawPolygon(poly.points);
-  graphics.endFill();
-
-  game.physics.enable(graphics, Phaser.Physics.ARCADE);
-  graphics.body.immovable = true;
-  graphics.body.allowGravity = false;
-
-  ship.body.maxVelocity.setTo(250, 250);
+  // game.physics.enable(graphics, Phaser.Physics.P2JS);
+  // graphics.body.immovable = true;
+  // graphics.body.allowGravity = false;
+  // ship.body.maxVelocity.setTo(250, 250);
   
   game.input.keyboard.addKeyCapture([
     Phaser.Keyboard.LEFT,
@@ -49,26 +52,15 @@ function create () {
 
 function update() {
 
-  game.physics.arcade.collide(ship, graphics);
-
-  if (ship.x > game.width) ship.x = 0;
-  if (ship.x < 0) ship.x = game.width;
-  if (ship.y > game.height) ship.y = 0;
-  if (ship.y < 0) ship.y = game.height;
-
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    ship.body.angularVelocity = -180;
+    ship.body.rotateLeft(100);
   } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    ship.body.angularVelocity = 180;
+    ship.body.rotateRight(100);
   } else {
-    ship.body.angularVelocity = 0;
+    ship.body.setZeroRotation();
   }
 
   if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    const ACCELERATION = 200;
-    ship.body.acceleration.x = Math.sin(ship.rotation) * ACCELERATION;
-    ship.body.acceleration.y = -Math.cos(ship.rotation) * ACCELERATION;
-  } else {
-    ship.body.acceleration.setTo(0, 0);
+    ship.body.thrust(200)
   }
 }
